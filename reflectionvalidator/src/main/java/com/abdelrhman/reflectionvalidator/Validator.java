6d5f7ac075f7ccc712a48c59f1d0ad8c8e26b455
Abdelrhman Walid <abdelrhmanwalid1@gmail.com>
2017-08-24 18:11:07 +0200

    fix validation logic break if one validation is failed

--- reflectionvalidator/src/main/java/com/abdelrhman/reflectionvalidator/Validator.java
+++ reflectionvalidator/src/main/java/com/abdelrhman/reflectionvalidator/Validator.java
@@ -20,18 +20,27 @@ public class Validator {
         boolean valid = true;
         Field[] fields = activity.getClass().getDeclaredFields();
         for (Field field : fields) {
-            if (field.isAnnotationPresent(NotEmpty.class) && EditText.class.equals(field.getType())) {
+            if (EditText.class.equals(field.getType())) {
                 for (Annotation annotation : field.getDeclaredAnnotations()) {
+                    System.out.println(annotation.toString());
                     if (annotation instanceof NotEmpty) {
-                        NotEmpty notEmpty = ((NotEmpty) annotation);
                         EditText editText = getEditTextFromField(field, activity);
-                        valid = validateNotEmpty(editText, notEmpty);
+                        boolean isNotEmptyValid = validateNotEmpty(editText, ((NotEmpty) annotation));
+                        valid = valid && isNotEmptyValid;
+                        if (!isNotEmptyValid)
+                            break;
                     } else if (annotation instanceof Max) {
                         EditText editText = getEditTextFromField(field, activity);
-                        valid = valid && validateMax(editText, ((Max) annotation));
+                        boolean isMaxValid = validateMax(editText, ((Max) annotation));
+                        valid = valid && isMaxValid;
+                        if (!isMaxValid)
+                            break;
                     } else if (annotation instanceof Min) {
                         EditText editText = getEditTextFromField(field, activity);
-                        valid = valid && validateMin(editText, ((Min) annotation));
+                        boolean isMinValid = validateMin(editText, ((Min) annotation));
+                        valid = valid && isMinValid;
+                        if (!isMinValid)
+                            break;
                     }
                 }
             }
@@ -42,23 +51,21 @@ public class Validator {
     private static boolean validateNotEmpty(EditText editText, NotEmpty notEmpty) {
         if (TextUtils.isEmpty(editText.getText())) {
             editText.setError(notEmpty.errorMessage());
-            System.out.println("not valid");
             return false;
         } else {
             editText.setError(null);
-            System.out.println("valid");
             return true;
         }
     }
 
     private static boolean validateMax(EditText editText, Max max) {
-        boolean valid = editText.getText().length() > max.value();
+        boolean valid = editText.getText().length() <= max.value();
         editText.setError(valid ? null : max.errorMessage());
         return valid;
     }
 
     private static boolean validateMin(EditText editText, Min min) {
-        boolean valid = editText.getText().length() < min.value();
+        boolean valid = editText.getText().length() >= min.value();
         editText.setError(valid ? null : min.errorMessage());
         return valid;
     }
