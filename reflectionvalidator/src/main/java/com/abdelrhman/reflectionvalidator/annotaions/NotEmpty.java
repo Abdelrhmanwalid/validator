9e9953088146cd106668c53f2e3b7ae2c05c92c2
Abdelrhman Walid <abdelrhmanwalid1@gmail.com>
2017-08-24 15:12:19 +0200

    update javadoc

--- reflectionvalidator/src/main/java/com/abdelrhman/reflectionvalidator/annotaions/NotEmpty.java
+++ reflectionvalidator/src/main/java/com/abdelrhman/reflectionvalidator/annotaions/NotEmpty.java
@@ -5,12 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * check if an {@link android.widget.EditText} has empty text
+ * example
+ * <pre>
+ * {@code @NotEmpty EditText editText;}
+ * </pre>
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotEmpty {
 
     /**
-     * @return error message to be displayed, defaults to an empty string
+     * @return error message to be displayed, defaults to "can not be empty"
      */
     String errorMessage() default "can not be empty";
 
